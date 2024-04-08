257a8eaa85aaf09cc335997a4678f401e5feadc6
erenkaraboga <eren.karaboga@naylalabs.com>
2024-04-08 15:41:02 +0300

    Refactor Repository Module with repositories

---
+++ app/src/main/java/com/week4/getir/apicallexample/RepositoryModule.kt
@@ -0,0 +1,18 @@
+package com.week4.getir.apicallexample
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideUserRepository(apiService: ApiClient): UserRepository =
+        UserRepositoryImpl(apiService)
+
+}
