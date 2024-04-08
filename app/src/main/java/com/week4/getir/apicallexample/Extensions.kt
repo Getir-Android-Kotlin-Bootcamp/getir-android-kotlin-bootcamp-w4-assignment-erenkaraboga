be02db5d8317e2a7708c59745fe1d48a36a9017d
erenkaraboga <eren.karaboga@naylalabs.com>
2024-04-08 15:42:22 +0300

    Update extensions

---
+++ app/src/main/java/com/week4/getir/apicallexample/Extensions.kt
@@ -0,0 +1,16 @@
+package com.week4.getir.apicallexample
+
+import org.json.JSONObject
+
+fun String.combineWithPath(path: String): String {
+    val baseUrl = if (this.endsWith("/")) this else "$this/"
+
+    val adjustedPath = if (path.startsWith("/")) path.substring(1) else path
+    return baseUrl + adjustedPath
+}
+fun LoginRequestModel.toJsonObject(): JSONObject {
+    val jsonObject = JSONObject()
+    jsonObject.put("email", this.email)
+    jsonObject.put("password", this.password)
+    return jsonObject
+}
