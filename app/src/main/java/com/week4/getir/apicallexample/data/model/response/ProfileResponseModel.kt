e51b95c3f3c5331019466290f3672e6af11d01e0
erenkaraboga <eren.karaboga@naylalabs.com>
2024-04-11 02:18:05 +0300

    Add profile response model

---
+++ app/src/main/java/com/week4/getir/apicallexample/data/model/response/ProfileResponseModel.kt
@@ -0,0 +1,15 @@
+package com.week4.getir.apicallexample.data.model.response
+
+data class ProfileResponseModel(
+    val country: Any,
+    val email: String,
+    val employer: Any,
+    val fullName: String,
+    val id: Int,
+    val latitude: Double,
+    val longitude: Double,
+    val occupation: Any,
+    val password: String,
+    val phoneNumber: Any,
+    val userId: String
+)
