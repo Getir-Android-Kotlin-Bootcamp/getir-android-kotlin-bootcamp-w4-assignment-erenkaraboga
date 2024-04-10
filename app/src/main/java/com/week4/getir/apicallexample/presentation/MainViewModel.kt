ee6573410f000221641e6c39454f8261782fd799
erenkaraboga <eren.karaboga@naylalabs.com>
2024-04-11 02:20:06 +0300

    Update main view model for profile request

--- app/src/main/java/com/week4/getir/apicallexample/presentation/MainViewModel.kt
+++ app/src/main/java/com/week4/getir/apicallexample/presentation/MainViewModel.kt
@@ -3,8 +3,10 @@ package com.week4.getir.apicallexample.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.week4.getir.apicallexample.common.Resource
-import com.week4.getir.apicallexample.data.model.LoginRequestModel
+import com.week4.getir.apicallexample.data.model.request.LoginRequestModel
+import com.week4.getir.apicallexample.data.model.response.ProfileResponseModel
 import com.week4.getir.apicallexample.domain.use_cases.LoginUseCase
+import com.week4.getir.apicallexample.domain.use_cases.ProfileUseCase
 
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
@@ -12,7 +14,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val loginUseCase: LoginUseCase
+    private val loginUseCase: LoginUseCase,
+    private val profileUseCase: ProfileUseCase
 
 
 ) : ViewModel() {
@@ -22,6 +25,9 @@ class MainViewModel @Inject constructor(
     fun getUserState(): StateFlow<UserViewState> = _userState.asStateFlow()
 
 
+    private val _profileState = MutableStateFlow<ProfileViewState>(ProfileViewState.Init)
+
+    fun getProfileState(): StateFlow<ProfileViewState> = _profileState.asStateFlow()
     init {
         getMyUser()
     }
@@ -42,6 +48,21 @@ class MainViewModel @Inject constructor(
 
         }.launchIn(viewModelScope)
     }
+     fun getProfile(userId : String) {
+        profileUseCase.invoke(userId).onEach {
+            when (it) {
+                is Resource.Error -> {
+                    _profileState.value = ProfileViewState.Error(it.message)
+                }
+                is Resource.Loading -> {
+                }
+                is Resource.Success -> {
+                    _profileState.value = ProfileViewState.Success(it.data)
+                }
+            }
+
+        }.launchIn(viewModelScope)
+    }
 
 
     sealed class UserViewState {
@@ -49,5 +70,11 @@ class MainViewModel @Inject constructor(
         data class Success(val data: String?) : UserViewState()
         data class IsLoading(val isLoading: Boolean) : UserViewState()
         data class Error(val error: String) : UserViewState()
+    }
+    sealed class ProfileViewState {
+        data object Init : ProfileViewState()
+        data class Success(val data: ProfileResponseModel?) : ProfileViewState()
+        data class IsLoading(val isLoading: Boolean) : ProfileViewState()
+        data class Error(val error: String) : ProfileViewState()
     }
 }
