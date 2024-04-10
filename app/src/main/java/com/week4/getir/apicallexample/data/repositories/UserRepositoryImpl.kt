62df3c037595d4123b9daa1254e5906ff081d0f1
erenkaraboga <eren.karaboga@naylalabs.com>
2024-04-11 02:19:16 +0300

    Update user repository for profile request

--- app/src/main/java/com/week4/getir/apicallexample/data/repositories/UserRepositoryImpl.kt
+++ app/src/main/java/com/week4/getir/apicallexample/data/repositories/UserRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.week4.getir.apicallexample.data.repositories
 
-import com.week4.getir.apicallexample.data.model.LoginRequestModel
+import com.week4.getir.apicallexample.data.model.request.LoginRequestModel
+import com.week4.getir.apicallexample.data.model.response.ProfileResponseModel
 import com.week4.getir.apicallexample.data.remote.ApiClient
 import com.week4.getir.apicallexample.domain.repositories.UserRepository
 import javax.inject.Inject
@@ -8,6 +9,10 @@ import javax.inject.Inject
 class UserRepositoryImpl @Inject constructor(private val api: ApiClient) : UserRepository {
     override suspend fun login(loginRequestModel: LoginRequestModel): String {
        return api.login(loginRequestModel)
+    }
+
+    override suspend fun getProfile(userId: String): ProfileResponseModel? {
+      return api.getProfile(userId)
     }
 
 
--- app/src/main/java/com/week4/getir/apicallexample/domain/repositories/UserRepository.kt
+++ app/src/main/java/com/week4/getir/apicallexample/domain/repositories/UserRepository.kt
@@ -1,7 +1,9 @@
 package com.week4.getir.apicallexample.domain.repositories
 
-import com.week4.getir.apicallexample.data.model.LoginRequestModel
+import com.week4.getir.apicallexample.data.model.request.LoginRequestModel
+import com.week4.getir.apicallexample.data.model.response.ProfileResponseModel
 
 interface UserRepository {
     suspend fun login(loginRequestModel: LoginRequestModel): String
+    suspend fun getProfile(userId: String): ProfileResponseModel?
 }
